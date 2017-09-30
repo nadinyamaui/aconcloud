f9b65ed4107b82256bc73d50f5b872c6f987ffcc
Nadin Yamaui <nadin@caagcrm.com>
2017-09-30 00:08:04 -0400

    Updated redis database

--- config/database.php
+++ config/database.php
@@ -97,7 +97,7 @@ return [
         'default' => [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'port'     => env('REDIS_PORT', 6379),
-            'database' => 0,
+            'database' => env('REDIS_DB_NUM', 1),
             'password' => env('REDIS_PASSWORD', null),
         ],
 
